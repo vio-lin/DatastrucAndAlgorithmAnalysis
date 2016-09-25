0385a2b3e152a98d68725a02218cdbaf70434bf2
vio-lin <846271633@qq.com>
2016-09-25 22:38:47 +0800

    递归的陷阱

--- DatastrucAndAlgorithmAnalysis/src/com/DataStructAndAlgorth/Sort/定长字符串的计数基数排序.java
+++ DatastrucAndAlgorithmAnalysis/src/com/DataStructAndAlgorth/Sort/定长字符串的计数基数排序.java
@@ -14,12 +14,16 @@ public class 定长字符串的计数基数排序 {
 		
 		String[] in = arr;
 		String[] out = buffer;
-		
+		//从字符串的最后开始慢慢往前面 算  因为最大位上的数组 一定优先决定最后的排序
 		for(int pos = stringlen-1;pos>=0;pos--){
+			//设置使用count 来保存每个组 index 这样可以在数组中减少考虑位置
 			int[] count = new int[BUCKETS+1];
+		
+			
 			for(int i=0;i<N;i++)
 				count[in[i].charAt(pos)+1]++;
 			
+			//count 保存当亲的ascll码建立 的bucket的index 当赋值结束后 这边的容量要++ 
 			for(int b=1;b<=BUCKETS;b++)
 				count[b]+=count[b-1];
 		
